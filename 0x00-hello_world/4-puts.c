7665bf5c7cf8740c90067f81346bc396d33f9a93
PatDziks <patdziks4050@gmail.com>
2023-02-16 14:42:21 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is describing the main function
+ *
+ * Return: always bings out 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+}

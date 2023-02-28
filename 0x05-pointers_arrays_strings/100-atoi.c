727a09263c51dd1b352d04e058872cea3553ea48
PatDziks <patdziks4050@gmail.com>
2023-02-27 20:46:50 -0800

    task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,26 +1,34 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include "main.h"
 
 /**
- * main - generates keygen.
+ * _atoi - converts a string to an integer.
+ * @s: input string.
  *
- * Return: 0 Always.
+ * Return: integer.
  */
-int main(void)
+int _atoi(char *s)
 {
-int r = 0, c = 0;
-time_t t;
+unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
 
-srand((unsigned int) time(&t));
-while (c < 2772)
+while (*(s + count) != '\0')
 {
-r = rand() % 128;
-if ((c + r) > 2772)
+if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
 break;
-c = c + r;
-printf("%c", r);
+if (*(s + count) == '-')
+pn *= -1;
+if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+{
+if (size > 0)
+m *= 10;
+size++;
 }
-printf("%c\n", (2772 - c));
-return (0);
+count++;
 }
+for (i = count - size; i < count; i++)
+{
+oi = oi + ((*(s + i) - 48) * m);
+m /= 10;
+}
+return (oi *pn);
+}
+
